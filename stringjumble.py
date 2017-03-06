23dc6a6ff3883c7f8779ea4768795270153c364c
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-03-06 14:24:55 -0500

    Update stringjumble.py

--- stringjumble.py
+++ stringjumble.py
@@ -21,7 +21,13 @@ Output of your program should look like this:
 
 Please enter a string of text (the bigger the better): There are a few techniques or tricks that you may find handy
 You entered "There are a few techniques or tricks that you may find handy". Now jumble it:
-ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
-handy find may you that tricks or techniques few a are There
-erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
+backward-backward: ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
+forward-backward: handy find may you that tricks or techniques few a are There
+backward-forward: erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+#forward-backward
+sentence = input("Please enter a string of text (the bigger the better): ")
+a2 = sentence.split()
+b2 = a2.sort()
+c2 = " ".join(b2)
+print(c2)
