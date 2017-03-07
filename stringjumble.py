76e7906c4725055fb9f807f617a6a0cd16d99adc
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-03-07 14:23:39 -0500

    Updated from Brython Server: 3/7/2017 2:23:39 PM

--- stringjumble.py
+++ stringjumble.py
@@ -24,7 +24,7 @@ You entered "There are a few techniques or tricks that you may find handy". Now 
 backward-backward: ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 forward-backward: handy find may you that tricks or techniques few a are There
 backward-forward: erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
-"""
+
 sentence = input("Please enter a string of text (the bigger the better): ")
 
 #backward-backward:
@@ -36,14 +36,29 @@ for a in mylist1:
     print(a1[-a], end="")
 print(" ")
 
-
-
 #forward-backward:
 a2 = sentence.split()
 length2 = len(a2)
 mylist2 = range(1, int(length2)+1)
 for b in mylist2:
     print(a2[-b], end=" ")
+"""
+#backward-forward
+    
+sentence = input("Please enter a string of text (the bigger the better): ")
+
+a3=sentence.split()
+
+for c in a3:
+    words=list(c)
+    length3=len(words)
+    mylist3=range(1, int(length3)+1)
+    for d in mylist3:
+        print(words[-d], end="")
+    print(" ", end="")
+
+        
+    
     
 
 
